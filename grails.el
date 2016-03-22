3bdab0522b29a7f7206cca78419dae368f1c69c5
Alessandro Miliucci <lifeisfoo@gmail.com>
2016-03-22 19:11:20 +0100

    bump version 0.3.1 more robust navigation

--- grails.el
+++ grails.el
@@ -3,7 +3,7 @@
 ;; Copyright (c) 2016 Alessandro Miliucci
 ;;
 ;; Authors: Alessandro Miliucci <lifeisfoo@gmail.com>
-;; Version: 0.3.0
+;; Version: 0.3.1
 ;; URL: https://github.com/lifeisfoo/emacs-grails
 ;; Package-Requires: ((emacs "24"))
 
