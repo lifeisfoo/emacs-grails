539d1f65e62f17f1e86733fc3f00627343827c8f
Alessandro Miliucci <lifeisfoo@gmail.com>
2016-03-23 13:48:38 +0100

    updated mode description

--- grails.el
+++ grails.el
@@ -29,40 +29,41 @@
 ;; Grails.el is a minor mode that allows an easy
 ;; navigation of Gails projects.  It allows jump to a model, to a view,
 ;; to a controller or to a service.
-;;
+
 ;; For more details, see the project page at
+
 ;; https://github.com/lifeisfoo/emacs-grails
-;;
+
 ;; Installation:
-;;
+
 ;; Copy this file to to some location in your Emacs load path.  Then add
 ;; "(require 'grails)" to your Emacs initialization (.emacs,
 ;; init.el, or something).
-;;
+
 ;; Example config:
-;;
+
 ;;   (require 'grails)
 
-;; Then, to auto enable grails mode, create a .dir-locals.el file
+;; To auto enable grails minor mode, create a .dir-locals.el file
 ;; in the root of the grails project with this configuration:
 
-;; ((groovy-mode (grails . 1))
-;;  (html-mode (grails . 1))
-;;  (java-mode (grails . 1)))
+;;    ((nil . ((grails . 1))))
+
+;; In this way, the grails minor mode will be always active inside your project tree.
+;; The first time that this code is executed, Emacs will show a security
+;; prompt: answer "!" to mark code secure and save your decision (a configuration
+;; line is automatically added to your .emacs file).
+
+;; Otherwise, if you want to have grails mode auto enabled only
+;; when using certain major modes, place this inside your `.dir-locals.el`:
+
+;;     ((groovy-mode (grails . 1))
+;;     (html-mode (grails . 1))
+;;     (java-mode (grails . 1)))
 
 ;; In this way, the grails mode will be auto enabled when any of
-;; these major modes are loaded (only in this directory tree - the project tree)
+;; these modes are loaded (only in this directory tree - the project tree)
 ;; (you can attach it to other modes if you want).
-
-;; The first time that this code is executed, Emacs will show a security
-;; prompt: answer "!" to mark code secure and save your decision.
-;; (a configuration line is automatically added to your .emacs file)
-
-;; In order to have grails minor mode always enabled inside your project tree,
-;; place inside your `.dir-locals.el`:
-
-;;   ((nil . ((grails . 1))))
-;;
 
 ;;; Code:
 
